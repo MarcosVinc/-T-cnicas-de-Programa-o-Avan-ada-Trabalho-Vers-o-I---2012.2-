c6a31db2cba7deb0e170e4d25bc8e7d9811bb38c
Marcos Vinicius <marcos.viniciuswac@outlook.com>
2023-02-24 00:50:26 -0300

    Att, fazendo as classes principais!

---
+++ Sistema eletrônico de gerenciamento/Entities/Produtos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_eletrônico_de_gerenciamento.Entities
+{
+    class Produtos
+    {
+    }
+}
---
+++ Sistema eletrônico de gerenciamento/Repositorio/Entities/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Usuario
+    {
+        protected string error;
+        public string Nome { get; set; }
+        public string Senha {  get; private set; }
+
+        public Usuario(string nome, string senha) 
+        {
+            try
+            {
+                if (nome != "" || senha != "")
+                {
+                    Nome = nome;
+                    Senha = senha;
+                }
+            }
+            catch (Exception e) 
+            {
+                error = e.Message;
+            }
+          
+            
+
+        }
+    }
+}
